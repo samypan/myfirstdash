d9db7fc01b4b1135d2c9db6bc634ad4bab789cdb
Sameer Pandey <samirpandey72@gmail.com>
2021-10-13 13:50:48 +1100

    Added selection and coverart viewing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 //import { Box, List, ListItem } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import "bulma/css/bulma.min.css";
+//import docstyle from "./App.css";
+
 function App() {
   const [cast, setCast] = useState([]);
+  const [image, setImage] = useState();
   const apiKey = "d2d1e21e8a8e48f592b0deb6d48f6571";
   //  const [loading, setLoading] = useState();
   const getCast = () => {
@@ -15,23 +18,37 @@ function App() {
     getCast();
   }, []);
 
+  const handleClick = (i) => {
+    setImage(i);
+  };
   return (
     <div>
-      {cast
-        .filter((title) => title.id < 11)
-        .map((home) => (
-          <section className="buttons">
-            <h1 className="button is-dark is-outlined is-rounded" key={home.id}>
+      <section className=" buttons ">
+        {cast
+          .filter((title) => title.id < 11)
+          .map((home) => (
+            <div
+              className=" button is-dark is-outlined is-rounded "
+              key={home.id}
+              onClick={() => handleClick(home.images[0].remoteUrl)}
+            >
               {home.title}
-            </h1>
-            {/* <img
-            src={home.images[0].remoteUrl}
-            alt={home.title}
-            width="150"
-            height="150"
-          /> */}
-          </section>
-        ))}
+            </div>
+          ))}
+      </section>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        Select a movie title to see the cover-art.
+        <div class="box">
+          <img src={image} width="200"></img>
+        </div>
+      </div>
     </div>
   );
 }
--- src/NavBar.js
+++ src/NavBar.js
@@ -2,7 +2,14 @@ import React from "react";
 import "bulma/css/bulma.min.css";
 function NavBar() {
   return (
-    <div style={{ marginBottom: 10 }}>
+    <div
+      style={{
+        marginBottom: 10,
+        dislay: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <nav
         className="navbar is-dark"
         role="navigation"
